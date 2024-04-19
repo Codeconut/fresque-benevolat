a5e1ecafbfbe6e93b0c85decf57f3d6e53d5d74a
NVaissaud <n.vaissaud@gmail.com>
2024-04-19 17:59:15 +0200

    Test Slack Interactivity

---
+++ app/Http/Controllers/Api/SlackController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SlackController extends Controller
+{
+    public function interactivity(Request $request)
+    {
+        $payload = json_decode($request->payload);
+
+        // ray($payload);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Http\Controllers\Api\SlackController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+// Route::post('/slack/interactivity', [SlackController::class, 'interactivity']);
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
