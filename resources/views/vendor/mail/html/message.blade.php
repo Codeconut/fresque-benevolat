f5fa7b22825fe2327b4ab7fa4212e5e559450c57
NVaissaud <n.vaissaud@gmail.com>
2024-04-19 16:02:10 +0200

    Theme footer mail

--- resources/views/vendor/mail/html/message.blade.php
+++ resources/views/vendor/mail/html/message.blade.php
@@ -1,22 +1,40 @@
 <x-mail::layout>
-    <x-slot:header>
-        <x-mail::header :url="config('app.url')">
-            <img src="{{ asset('images/logos/fresque-benevolat-logo.svg') }}" alt="Logo de la fresque du bénévolat"
-                width="200">
-        </x-mail::header>
-    </x-slot:header>
-    {{ $slot }}
-    @isset($subcopy)
-        <x-slot:subcopy>
-            <x-mail::subcopy>
-                {{ $subcopy }}
-            </x-mail::subcopy>
-        </x-slot:subcopy>
-    @endisset
-    <x-slot:footer>
-        <x-mail::footer>
-            Cet e-mail a été envoyé suite à votre inscription à une fresque du Bénévolat sur
-            fresquedubenevolat.jeveuxaider.gouv.fr
-        </x-mail::footer>
-    </x-slot:footer>
+<x-slot:header>
+<x-mail::header :url="config('app.url')">
+<img src="{{ asset('images/logos/fresque-benevolat-logo.svg') }}" alt="Logo de la fresque du bénévolat"
+width="200">
+</x-mail::header>
+</x-slot:header>
+{{ $slot }}
+@isset($subcopy)
+<x-slot:subcopy>
+<x-mail::subcopy>
+{{ $subcopy }}
+</x-mail::subcopy>
+</x-slot:subcopy>
+@endisset
+<x-slot:footer>
+<x-mail::footer>
+
+<div class="footer-links">
+
+[Trouver une mission](https://jeveuxaider.gouv.fr/missions-benevolat)
+[Toutes les fresques du bénévolat](#)
+
+</div>
+
+<div class="footer-socials">
+
+[![Instagram]({{ asset('images/mail/instagram.svg') }})](https://www.instagram.com/jeveuxaider_gouv/?hl=fr)
+[![Facebook]({{ asset('images/mail/facebook.svg') }})](https://www.facebook.com/jeveuxaider.gouv.fr/)
+[![Twitter]({{ asset('images/mail/twitter.svg') }})](https://twitter.com/ReserveCivique)
+[![Linkedin]({{ asset('images/mail/linkedin.svg') }})](https://www.linkedin.com/company/jeveuxaider-gouv-fr?originalSubdomain=fr)
+
+</div>
+
+Cet e-mail a été envoyé suite à votre inscription à une fresque du Bénévolat sur
+fresquedubenevolat.jeveuxaider.gouv.fr
+
+</x-mail::footer>
+</x-slot:footer>
 </x-mail::layout>
