7eab259ffafd9f465f0e5b6d719e3e429e64044c
NVaissaud <n.vaissaud@gmail.com>
2024-04-22 13:46:30 +0200

    cors config

--- config/filesystems.php
+++ config/filesystems.php
@@ -39,7 +39,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
             'throw' => false,
         ],
@@ -53,6 +53,17 @@ return [
             'url' => env('AWS_URL'),
             'endpoint' => env('AWS_ENDPOINT'),
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
+            'throw' => false,
+        ],
+
+        'do-spaces' => [
+            'driver' => 's3',
+            'key' => env('DIGITALOCEAN_SPACES_KEY'),
+            'secret' => env('DIGITALOCEAN_SPACES_SECRET'),
+            'region' => env('DIGITALOCEAN_SPACES_REGION'),
+            'bucket' => env('DIGITALOCEAN_SPACES_BUCKET'),
+            'endpoint' => env('DIGITALOCEAN_SPACES_ENDPOINT'),
+            // 'root' => env('DIGITALOCEAN_SPACES_ROOT_FOLDER', 'local'),
             'throw' => false,
         ],
 
