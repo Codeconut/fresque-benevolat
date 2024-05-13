febcf539e80ada1e6478e006b7efac5bfdcbc389
IJahman <kevin.vaissaud@gmail.com>
2024-05-13 17:09:03 +0200

    revert vite config

--- vite.config.js
+++ vite.config.js
@@ -3,25 +3,21 @@ import laravel from 'laravel-vite-plugin'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
-export default defineConfig(({ mode }) => {
-  return {
-    base: mode === 'production' ? '/fresque-benevolat/build/' : '/',
-    plugins: [
-      vueJsx(),
-      laravel({
-        input: 'resources/js/app.js',
-        ssr: 'resources/js/ssr.js',
-        refresh: true,
-      }),
-      vue({
-        template: {
-          transformAssetUrls: {
-            base:
-              mode === 'production' ? 'https://www.jeveuxaider.gouv.fr/fresque-benevolat/' : null,
-            includeAbsolute: mode === 'production' ? true : false,
-          },
+export default defineConfig({
+  plugins: [
+    vueJsx(),
+    laravel({
+      input: 'resources/js/app.js',
+      ssr: 'resources/js/ssr.js',
+      refresh: true,
+    }),
+    vue({
+      template: {
+        transformAssetUrls: {
+          base: null,
+          includeAbsolute: false,
         },
-      }),
-    ],
-  }
+      },
+    }),
+  ],
 })
