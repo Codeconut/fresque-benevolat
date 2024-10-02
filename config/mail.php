a2930b982c1f54ca1fc3b9e2692babe319912541
NVaissaud <n.vaissaud@gmail.com>
2024-10-02 09:53:23 +0200

    Reply to

--- config/mail.php
+++ config/mail.php
@@ -112,4 +112,9 @@ return [
         'name' => env('MAIL_FROM_NAME', 'Example'),
     ],
 
+    'reply_to' => [
+        'address' => env('MAIL_REPLY_TO_ADDRESS', 'hello@example.com'),
+        'name' => env('MAIL_REPLY_TO_NAME', 'Support - La Fresque du Bénévolat'),
+    ],
+
 ];
