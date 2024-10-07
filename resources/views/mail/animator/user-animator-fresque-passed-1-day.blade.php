38a0f601b77eff1d62e870102e1917e9db5f702d
NVaissaud <n.vaissaud@gmail.com>
2024-10-07 11:24:33 +0200

    Link in notification animator fresque passed 1 day

--- resources/views/mail/animator/user-animator-fresque-passed-1-day.blade.php
+++ resources/views/mail/animator/user-animator-fresque-passed-1-day.blade.php
@@ -7,7 +7,7 @@ Pour clôturer cette belle session, il te reste quelques actions simples à effe
 
 1. **Mettre à jour la présence des participants** directement sur le site si tu ne l’as pas encore fait : <a href="{{ $url }}">{{ $url }}</a>
 2. **Crée un dossier** sur [ce Drive](https://drive.google.com/drive/folders/1JddaSS8AKWbmn5jgb9gkIx6uwF-LMiq4), en suivant la nomenclature : AAA_MM_JJ_Lieu_Ville *(ex. : 2024_03_01_Hôtel_Pasteur_Rennes)*, et ajoute les photos de la session.
-3. **Partage la photo de groupe** et un retour d'expérience d'un(e) participant(e) sur l'**espace de discussion dédié aux animateur(rice)s**.
+3. **Partage la photo de groupe** et un retour d'expérience d'un(e) participant(e) sur [l'**espace de discussion dédié aux animateur(rice)s**](https://chat.whatsapp.com/JCGEYUlaLqSA3DW4vjnNoN).
 4. **Valorise ton expérience** sur les réseaux sociaux. Pour t’aider, nous avons rédigé des posts prêts à l’emploi disponibles ici
 
 Si tu as des retours ou des questions après la Fresque du Bénévolat, tu peux m’écrire, j’y répondrais avec plaisir. 
